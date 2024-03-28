580fc13daf47715cca7d5d96ccc5f373760f5927
kliko <kliko@softalix.com>
2024-03-28 15:12:22 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AppConfig from './config/app'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log(AppConfig.IMAGES_API_ENDPOINT)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="w-24 aspect-square animate-spin" alt="logo"/>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+
+                {/*<ImageCarousel/>*/}
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/config/app.ts
@@ -0,0 +1,5 @@
+const AppConfig = {
+    IMAGES_API_ENDPOINT: process.env.REACT_APP_IMAGE_API_URL
+}
+
+export default AppConfig
