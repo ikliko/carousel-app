8c16c270d0de8219d1bebc8033462c096c9d43e9
kliko <kliko@softalix.com>
2024-03-31 15:08:14 +0300

    img loader

--- src/components/InfiniteCarouselImage.tsx
+++ src/components/InfiniteCarouselImage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {CarouselImage} from '../Interfaces/CarouselImage'
 
-const InfiniteCarouselImage = ({ image, cssClass }: {image: CarouselImage, cssClass: string}) => (
+const InfiniteCarouselImage = ({ image, cssClass, onLoad }: {image: CarouselImage, cssClass: string, onLoad?: () => void}) => (
     <img
         key={image.id}
         src={image.url}
+        onLoad={onLoad}
         alt={` ${image.id}`}
         className={cssClass}
         loading="lazy"
