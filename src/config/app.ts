8c72b102bf077b569c1f4f9aca9ce0fb2ea02cdd
kliko <kliko@softalix.com>
2024-04-01 12:29:13 +0300

    config exported to .env

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,5 @@
 const AppConfig = {
-    IMAGES_API_ENDPOINT: process.env.REACT_APP_IMAGE_API_URL,
+    IMAGES_API_ENDPOINT: process.env.REACT_APP_IMAGE_API_URL || 'https://www.retire.waboratory.online/images',
 }
 
 export default AppConfig
