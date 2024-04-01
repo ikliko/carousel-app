a93a43e12faeebf50f2405cb713f4ba23f9f2544
kliko <kliko@softalix.com>
2024-04-01 13:50:14 +0300

    refactor

--- src/components/InfiniteCarousel.tsx
+++ src/components/InfiniteCarousel.tsx
@@ -5,10 +5,15 @@ import InfiniteCarouselImage from './InfiniteCarouselImage'
 import {calculateScrollLeft, getFitClass, loadImages} from '../helpers/utils'
 
 const renderLoader = (show: boolean) => show && (
-    <div role="status"
-         className="h-128 w-screen flex items-center justify-center bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700 absolute top-0 right-0 left-0 bottom-0">
-        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
-             xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+    <div
+        role="status"
+        className="h-128 w-screen flex items-center justify-center bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700 absolute top-0 right-0 left-0 bottom-0"
+    >
+        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600"
+             aria-hidden="true"
+             xmlns="http://www.w3.org/2000/svg"
+             fill="currentColor"
+             viewBox="0 0 16 20">
             <path
                 d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
             <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
@@ -18,12 +23,12 @@ const renderLoader = (show: boolean) => show && (
 )
 
 const InfiniteCarousel: React.FC<ImageCarouselProps> = ({
-                                                            images,
-                                                            height = 'h-full',
-                                                            width = 'w-full',
-                                                            imageFitType = 'DEFAULT',
-                                                            customClass = ''
-                                                        }) => {
+    images,
+    height = 'h-full',
+    width = 'w-full',
+    imageFitType = 'DEFAULT',
+    customClass = ''
+}) => {
     const carouselRef = useRef<HTMLDivElement>(null)
     const currentThreshHold = useRef<number>(1)
     const timeoutRef = useRef<number | null>(null)
@@ -85,6 +90,7 @@ const InfiniteCarousel: React.FC<ImageCarouselProps> = ({
     useEffect(() => {
         window.addEventListener('mousemove', onMouseMove)
         window.addEventListener('mouseup', onMouseUp)
+
         return () => {
             window.removeEventListener('mousemove', onMouseMove)
             window.removeEventListener('mouseup', onMouseUp)
@@ -169,18 +175,24 @@ const InfiniteCarousel: React.FC<ImageCarouselProps> = ({
         >
             {renderLoader(isInitialLoadingImg)}
 
-            <InfiniteCarouselImage key={`img-${293478 + 1}`}
-                                   onLoad={() => {
-                                       console.log(12321)
-                                       requestAnimationFrame(startRender)
-                                   }}
-                                   image={{url: images[images.length - 1], id: 1298324}}
-                                   cssClass={slideClass}/>
-            {images.map(
-                (url, id) => <InfiniteCarouselImage key={`img-${id + 1}`} image={{url, id}} cssClass={slideClass}/>
-            )}
-            <InfiniteCarouselImage key={`img-${2293478 + 1}`} image={{url: images[0], id: 12983324}}
-                                   cssClass={slideClass}/>
+            <InfiniteCarouselImage
+                key={`img-${293478 + 1}`}
+                onLoad={() => {
+                    console.log(12321)
+                    requestAnimationFrame(startRender)
+                }}
+                image={{url: images[images.length - 1], id: 1298324}}
+                cssClass={slideClass}/>
+            {images.map((url, id) => (
+                <InfiniteCarouselImage
+                    key={`img-${id + 1}`}
+                    image={{url, id}}
+                    cssClass={slideClass}/>
+            ))}
+            <InfiniteCarouselImage
+                key={`img-${2293478 + 1}`}
+                image={{url: images[0], id: 12983324}}
+                cssClass={slideClass}/>
         </div>
     )
 }
